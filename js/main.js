e2a7a0134eface4b2b7f5b73c19d6702e4ccdd88
David Heyman <dave@axismaps.com>
2018-05-02 06:55:06 +0100

    Analytics events for email

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 var counter = 1;
 var skip = false;
 
-$( document ).ready( function(){
+$( document ).ready( function (){
   init_events();
 });
 
 function init_events()
 {	
-	$("#menu").click( function()
-	{
+	$("#menu").click( function () {
 		$( "nav #links" ).slideToggle();
 	});
 	
-	$( window ).resize( function()
-	{
+	$( window ).resize( function () {
 		if( $( window ).width() > 650 ) $( "nav #links" ).show();
 		else $( "nav #links" ).hide();
 	});
 
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		if ($(window).scrollTop() > 100) {
 			$('body').addClass('fixed-header');
 		} else {
 			$('body').removeClass('fixed-header');
 		}
+	});
+
+	$('a.email-event').click(function () {
+		ga('send', 'event', 'contact', 'email');
 	})
 }
