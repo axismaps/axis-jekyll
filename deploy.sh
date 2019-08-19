f49778982c01b313816eda47976c8b531064f15b
David Heyman <dave@axismaps.com>
2019-08-19 07:39:01 +0100

    Use bundle to execute build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-jekyll build
+bundle exec jekyll build
 aws s3 sync _site s3://www.axismaps.com --exclude="Gemfile*" --exclude="deploy.sh" --delete
 aws cloudfront create-invalidation --distribution-id E3UDPY742B4M5R --paths /\*
